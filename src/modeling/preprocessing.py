b1c22486a3eb9f165c182cf02fabfa3edfe3f54e
Quentin Vajou <q.vajou@gmail.com>
2020-04-28 08:05:03 +0200

    add features ride distace

--- src/modeling/preprocessing.py
+++ src/modeling/preprocessing.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 from multiprocessing import Pool, cpu_count
 import math
 import datetime
+from copy import deepcopy
 
 from src.infrastructure.settings import logger, DATASET_SAMPLING_FRACTION
 
@@ -20,12 +21,14 @@ class Preprocessing:
         self.continuous = dict_modeling_params['continuous']
         self.discrete = dict_modeling_params['discrete']
         self.df_d = None
+        self.df_m = None
+        self.columns_timestamp = dict_modeling_params['columns_timestamp']
 
-    def load_and_merge_datasets(self, frac=1):
-        if os.path.isfile('data/trusted/preprocessed_dataset'+str(DATASET_SAMPLING_FRACTION)+'.csv')\
-                and os.access('data/trusted/preprocessed_dataset'+str(DATASET_SAMPLING_FRACTION)+'.csv', os.R_OK):
+    def load_and_merge_datasets(self, frac=.1):
+        if os.path.isfile('data/trusted/preprocessed_dataset'+str(frac)+'.csv')\
+                and os.access('data/trusted/preprocessed_dataset'+str(frac)+'.csv', os.R_OK):
             is_preprocessed = True
-            df_m = pd.read_csv('data/trusted/preprocessed_dataset'+str(DATASET_SAMPLING_FRACTION)+'.csv')
+            df_m = pd.read_csv('data/trusted/preprocessed_dataset'+str(frac)+'.csv')
         else:
             is_preprocessed = False
             df_rr = pd.read_csv('data/raw/rideRequests.log')
@@ -38,6 +41,7 @@ class Preprocessing:
         self.df_d = pd.read_csv('data/raw/drivers.log')
         self.df_d = self.timestamp_preprocessing(self.df_d)
         self.df_d = self.df_d.sort_values('logged_at')
+
         logger.info("datasets loaded")
 
         return df_m, is_preprocessed
@@ -49,9 +53,15 @@ class Preprocessing:
         :return:
         """
         if 'logged_at' in df.columns:
-            df['logged_at'] = pd.to_datetime(df['logged_at'], unit='s')
+            try:
+                df['logged_at'] = pd.to_datetime(df['logged_at'], unit='s')
+            except ValueError as e:
+                pass
         if 'created_at' in df.columns:
-            df['created_at'] = pd.to_datetime(df['created_at'], unit='s')
+            try:
+                df['created_at'] = pd.to_datetime(df['created_at'], unit='s')
+            except Exception as e:
+                pass
         return df
 
     def feature_engineering(self, df):
@@ -66,15 +76,34 @@ class Preprocessing:
         df['ride_distance'] = df.apply(lambda row: self.distance_between_point(
             (row['origin_lat'], row['origin_lon']), (row['destination_lat'], row['destination_lon'])), axis=1)
 
-        df = self.parallelize(df, self.features_workshift_state)
-        df['workshift_rides_ratio'] = df[['workshift_rides_count', 'workshift_rides_max']].apply(
+        # necessary for distance features extraction
+        self.df_m = deepcopy(df)
+
+        df = self.parallelize(df, self.workshift_state_extraction)
+
+        df['workshift_rides_count'], df['workshift_rides_prefered'],\
+            df['workshift_mean_rides_duration'], df['workshift_sum_rides_duration'] =\
+            zip(*df[['list_rides_time', 'list_all_rides_time']].apply(
+                lambda row: self.features_workshift_state(row), axis=1))
+
+        df = self.convert_and_format_dataset(df)
+
+        df = self.parallelize(df, self.feature_ride_distance)
+        # df = self.feature_ride_distance(df)
+
+        df['workshift_rides_ratio'] = df[['workshift_rides_count', 'workshift_rides_prefered']].apply(
             lambda row: row[0]/row[1], axis=1)
+        df['workshift_duration_ratio'] = df[['workshift_duration', 'workshift_prefered_duration']].apply(
+            lambda row: row[0]/row[1], axis=1)
+        df['distance_ratio'] = df[['ride_distance_cumul', 'ride_distance_prefered']].apply(
+            lambda row: row[0]/row[1], axis=1)
+
         logger.info("feature engineering ended")
         return df
 
-    def features_workshift_state(self, df):
-        df['workshift_duration'], df['workshift_rides_count'], df['workshift_mean_rides_duration'],\
-        df['workshift_sum_rides_duration'], df['workshift_rides_max'] = \
+    def workshift_state_extraction(self, df):
+        df['workshift_duration'], df['workshift_prefered_duration'],\
+            df['list_rides_time'], df['list_all_rides_time'] = \
             zip(*df[['logged_at', 'driver_id']].progress_apply(lambda row: self.workshift_state(row), axis=1))
         return df
 
@@ -82,26 +111,54 @@ class Preprocessing:
         booking_request_time = row['logged_at']
         df_driver = self.df_d.loc[self.df_d['driver_id'] == row['driver_id'], :]
         duration_workshift = booking_request_time - df_driver.iloc[0, -2]
+        duration_prefered_workshift = df_driver.iloc[-1, -2] - df_driver.iloc[0, -2]
         list_rides_time = []
-        count_all_rides = 0
+        list_all_rides_time = []
         for index, row_d in df_driver.iterrows():
-            count_all_rides += 1
             if row_d['logged_at'] <= booking_request_time:
                 if row_d['new_state'] == 'began_ride':
                     begin = row_d['logged_at']
                 elif row_d['new_state'] == 'ended_ride':
                     end = row_d['logged_at']
                     list_rides_time.append(end-begin)
+                    list_all_rides_time.append(end-begin)
             else:
-                continue
-        count_ride = len(list_rides_time)
-        if count_ride > 0:
+                if row_d['new_state'] == 'began_ride':
+                    begin = row_d['logged_at']
+                elif row_d['new_state'] == 'ended_ride':
+                    end = row_d['logged_at']
+                    list_all_rides_time.append(end-begin)
+        return duration_workshift, duration_prefered_workshift, list_rides_time, list_all_rides_time
+
+    def features_workshift_state(self, row):
+        list_rides_time = row['list_rides_time']
+        list_all_rides_time = row['list_all_rides_time']
+        count_rides = len(list_rides_time)
+        count_all_rides = len(list_all_rides_time)
+        if count_rides > 0:
             mean_ride_duration = np.mean(list_rides_time)
             sum_ride_duration = np.sum(list_rides_time)
         else:
             mean_ride_duration = datetime.timedelta()
             sum_ride_duration = datetime.timedelta()
-        return duration_workshift, count_ride, mean_ride_duration, sum_ride_duration, count_all_rides
+        return count_rides, count_all_rides, mean_ride_duration, sum_ride_duration
+
+    def feature_ride_distance(self, df):
+        df['ride_distance_cumul'], df['ride_distance_prefered'], df['count_booking_requests_received'] = \
+            zip(*df[['logged_at', 'driver_id']].progress_apply(
+                lambda row: self.ride_distances(row), axis=1))
+        return df
+
+    def ride_distances(self, row):
+        distance_prefered = self.df_m.loc[(self.df_m['driver_accepted'] == True) &
+                                          (self.df_m['driver_id'] == row['driver_id']), 'ride_distance'].sum()
+        distance_cumul = self.df_m.loc[(self.df_m['logged_at'] < row['logged_at']) &
+                                       (self.df_m['driver_accepted'] == True) &
+                                       (self.df_m['driver_id'] == row['driver_id']), 'ride_distance'].sum()
+        count_booking_requests_received = self.df_m.loc[(self.df_m['logged_at'] == row['logged_at']) &
+                                                        (self.df_m['driver_id'] == row['driver_id']), 'request_id'].count()
+
+        return distance_cumul, distance_prefered, count_booking_requests_received
 
     def feature_driver_availability(self, df):
         df['is_driver_available'], df['driver_next_state'] = zip(*df[['logged_at', 'driver_id']].progress_apply(lambda row: self.is_driver_available(row), axis=1))
@@ -153,56 +210,66 @@ class Preprocessing:
         return data
 
     def convert_and_format_dataset(self, df):
-        if 'workshift_duration' in df.columns:
-            df['workshift_duration'] = df['workshift_duration'].apply(lambda x: x.seconds/60)
-        if 'workshift_mean_rides_duration' in df.columns:
-            df['workshift_mean_rides_duration'] = df['workshift_mean_rides_duration'].apply(lambda x: x.seconds/60)
-        if 'workshift_sum_rides_duration' in df.columns:
-            df['workshift_sum_rides_duration'] = df['workshift_sum_rides_duration'].apply(lambda x: x.seconds/60)
+        for col in self.columns_timestamp:
+            if col in df.columns:
+                df[col] = df[col].apply(lambda x: x.seconds/60)
         return df
 
+    def subset_by_iqr_range(self, df, column, whisker_width=1.5):
+        ''' remove outliers from continuous serie in DataFrame
+        :param df:
+            dataframe wit all data
+        :param column:
+            serie within dataframe to scan for outliers
+        :param whisker_width:
+            distance from the quartiles that separate inliers from outliers
+        :return:
+            complete dataframe with rows of outliers removed
+        :rtype:dataframe
+        '''
+        q1 = df[column].quantile(0.25)
+        q3 = df[column].quantile(0.75)
+        iqr = q3 - q1
+        # Apply filter with respect to IQR, including optional whiskers
+        # filter = (df[column] >= q1 - whisker_width * iqr) & (df[column] <= q3 + whisker_width * iqr)
+        filter = (df[column] <= q3 + whisker_width * iqr)
+        return df.loc[filter]
+
     def preprocessing_rules(self, df):
+        '''
+        processing rules is defined to apply specific business rules. they're not part of preprocessing since
+        they don't necessarly come from data exploration and modeling improvement.
+        :param df:
+        :return:
+        '''
         return df
 
     def prepare_dataset_for_training(self, df, save_processed_dataset=False):
         df = self.feature_engineering(df)
-        df = self.convert_and_format_dataset(df)
         df = self.preprocessing_rules(df)
         if save_processed_dataset:
-            df.to_csv('data/trusted/preprocessed_dataset'+str(DATASET_SAMPLING_FRACTION)+'.csv')
+            df.to_csv('data/trusted/preprocessed_dataset'+str(DATASET_SAMPLING_FRACTION)+'.csv', index=False)
         return df
 
-    def filter_dataset_for_training(self, df):
+    def filter_dataset_for_training_column_wise(self, df):
         df_con = df.loc[:, self.continuous]
         df_cat = df.loc[:, self.discrete]
         df_cat = pd.get_dummies(df_cat)
         df = pd.concat([df_con, df_cat], axis=1)
         return df
 
-    def main(self):
-        pp = Preprocessing(dict_modeling_params)
-        df, _ = pp.load_and_merge_datasets()
-        # df = pp.prepare_dataset_for_training(df)
-        df_temp = df.loc[df['driver_id'] == "75D7A80A-1219-44A2-B0AD-795C0B773145", :]
-        df_temp = df_temp.sort_values(['logged_at'])
+    def filter_dataset_for_training_row_wise(self, df):
+        for iqr_col in self.continuous:
+            df = self.subset_by_iqr_range(df, iqr_col)
+        return df
 
-        row = df_temp.iloc[1]
-        booking_request_time = pd.to_datetime(row['logged_at'])
-        df_driver = pp.df_d.loc[pp.df_d['driver_id'] == row['driver_id'], :]
-        duration_workshift = booking_request_time - df_driver.iloc[0, -2]
-        list_rides_time = []
-        count_all_ride = 0
-        for index, row_d in df_driver.iterrows():
-            count_all_ride += 1
-            if row_d['logged_at'] <= booking_request_time:
-                if row_d['new_state'] == 'began_ride':
-                    begin = row_d['logged_at']
-                elif row_d['new_state'] == 'ended_ride':
-                    end = row_d['logged_at']
-                    list_rides_time.append(end-begin)
-            else:
-                continue
-        count_rides = len(list_rides_time)
-        mean_ride_duration = np.mean([])
-        sum_ride_duration = np.sum([])
-        sum_rides = None
+    def main(self):
+        self = Preprocessing(dict_modeling_params)
+        df, _ = self.load_and_merge_datasets(frac=.1)
+        # df = pp.prepare_dataset_for_training(df)
+        # driver_id = df.loc[12000, 'driver_id']
+        driver_id = df.loc[3400, 'driver_id']
+        df_temp = df.loc[df['driver_id'] == driver_id, :]
+        df_temp = df_temp.sort_values(['logged_at'])
+        df_driver = self.df_d.loc[self.df_d['driver_id'] == driver_id, :]
+
